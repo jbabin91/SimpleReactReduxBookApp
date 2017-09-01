eb1557ce46c2d57355f789f1adc37e83416758e0
Jace Babin <jbabin91@gmail.com>
2017-08-31 23:03:37 -0700

    Started by adding a books-reducer with a few book items

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
+'use strict'
 import { combineReducers } from 'redux'
 
+// Import reducers
+import BooksReducer from './reducer_books'
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 })
 
 export default rootReducer
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+'use strict'
+export default function () {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ]
+}
