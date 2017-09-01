d16cb5eb36630cdc34de8eb3e55fd14459a42c9c
Jace Babin <jbabin91@gmail.com>
2017-08-31 23:54:48 -0700

    Setup redux in book-list to connect all of the created books into the book-list container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,12 @@
 'use strict'
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li key='book.title' className='list-group-item'>
+        <li key={book.title} className='list-group-item'>
           {book.title}
         </li>
       )
@@ -19,3 +20,12 @@ export default class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps (state) {
+  // Whatever is returned will show up as props inside as BookList
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList)
